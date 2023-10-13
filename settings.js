a23918369e72ccd257defaaed5e035cdf5702546
kamran522 <107455935+kamran522@users.noreply.github.com>
2023-10-13 09:31:09 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -21,28 +21,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['923323963788'] //ur owner number
+global.ownernomer = "923323963788" //ur owner number2
+global.ownername = "Kamran Hasil" //ur owner name
+global.ytname = "K A M I" //ur yt chanel name
+global.socialm = "Insta: kamran_.hasil " //ur github or insta name
+global.location = "BALOCHISTAN" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "KAMI-MD8"
+global.ownernumber = '923323963788'
+global.ownername = 'Kamran Hasil'
+global.ownerNumber = ["923323963788@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@kamran"
+global.websitex = "https://youtu.be/kamran"
+global.wagc = "https://chat.whatsapp.com/CKsZELVzMrrIVI9TDqAu8X"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
-global.prefa = ['','!','.','#','&']
+global.wm = "KAMI-MD8."
+global.botscript = 'https://github.com/kamran522/KAMI-MD8' //script link
+global.packname = "Sticker By KAMI"
+global.author = "K A M I\n\n"
+global.creator = "923323963788@s.whatsapp.net"
+global.prefa = ['']
 global.sessionName = 'session'
 global.hituet = 0
 
